1c9a108a7a23e2e5e5c14b9653c88868742aff56
Valdes2 <52889938+Valdes2@users.noreply.github.com>
2019-10-18 12:07:16 +0300

    added changes to homework_31.1

---
+++ src/main/java/mate/academy/spring/dao/impl/BookDaoImpl.java
@@ -0,0 +1,47 @@
+package mate.academy.spring.dao.impl;
+
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
+import mate.academy.spring.dao.BookDao;
+import mate.academy.spring.entity.Author;
+import mate.academy.spring.entity.Book;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookDaoImpl implements BookDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public void addBook(Book book) {
+        sessionFactory.getCurrentSession().save(book);
+    }
+
+    @Override
+    public List<Book> findByTitle(String title) {
+        TypedQuery<Book> query = sessionFactory
+                .getCurrentSession().createQuery("from Book where title=:title");
+        query.setParameter("title", title);
+        return query.getResultList();
+    }
+
+    @Override
+    public List<Book> findByAuthor(Author author) {
+        TypedQuery<Book> query = sessionFactory
+                .getCurrentSession().createQuery("from Book where author=:author");
+        query.setParameter("author", author);
+        return query.getResultList();
+    }
+
+    @Override
+    public List<Book> listBooks() {
+        TypedQuery<Book> query = sessionFactory.getCurrentSession().createQuery("from Book");
+        return query.getResultList();
+    }
+}
--- src/main/java/mate/academy/spring/entity/User.java
+++ src/main/java/mate/academy/spring/entity/User.java
@@ -10,17 +10,14 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "users")
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
-
     @Column(name = "first_name")
     private String firstName;
-
     @Column(name = "last_name")
     private String lastName;
-
     @Column(name = "email")
     private String email;
 
